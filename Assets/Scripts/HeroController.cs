61075ce0511b2ba04c7e54255384a9ec64570aea
Andrés <angarg12@gmail.com>
2014-05-04 13:30:26 +0200

    Added sprites plus hero
    The basic sprites have been added to the project. A basic hero that
    moves and shoots has been created.

---
+++ Assets/Scripts/HeroController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary {
+	public float xMin; 
+	public float xMax; 
+	public float yMin;
+	public float yMax;
+}
+
+public class HeroController : MonoBehaviour {
+	public float speed;
+	public Boundary boundary;
+	
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	
+	private float nextFire;
+	
+	void Update (){
+		if (Input.GetButton("Fire1") && Time.time > nextFire) 
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+			//audio.Play ();
+		}
+	}
+	
+	void FixedUpdate (){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rigidbody2D.velocity = movement * speed;
+
+		transform.position = new Vector3(
+			Mathf.Clamp (transform.position.x, boundary.xMin, boundary.xMax), 
+			Mathf.Clamp (transform.position.y, boundary.yMin, boundary.yMax),
+			0.0f
+			);
+	}
+}
