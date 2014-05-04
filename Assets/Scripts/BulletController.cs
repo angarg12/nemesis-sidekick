9422f8e43cb99af3f5013d86d1e2faa5b3161775
Andrés <angarg12@gmail.com>
2014-05-04 13:59:50 +0200

    Hero shoot and game boundary
    The hero now shoots. A boundary sorrounds the game area so that objects
    that leave the boundary are destroyed.

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour {
+	public float speed;
+
+	void Start () {
+		rigidbody2D.velocity = transform.up * speed;
+	}
+}
