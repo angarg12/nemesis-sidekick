45a550139f6b673ac7f1f32b3cecc529f39c4f55
Andrés <angarg12@gmail.com>
2014-05-07 00:13:00 +0200

    Enemies killing each other fixed
    Enemies killing each ther has been fixed. Nevertheless will have to
    revisit the "unit" logic anyways, since it is becoming too convoluted
    and we don't know exactly what we want to do.

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -6,10 +6,14 @@ public class Unit : MonoBehaviour {
 		Faction myFaction = transform.GetComponent<Faction>();
 		Faction otherFaction = other.GetComponent<Faction>();
 		if(otherFaction != null){
-			if(FactionController.Damages(otherFaction.getColor(), myFaction.getColor())){
-				Destroy(gameObject);
+			// If we are not on the same category, destroy the other
+			if(gameObject.tag != other.tag){
+				// If we can damage each other, destroy myself
+				if(FactionController.Damages(otherFaction.getColor(), myFaction.getColor())){
+					Destroy(gameObject);
+				}
+				otherFaction.destroy(gameObject);
 			}
-			otherFaction.destroy(gameObject);
 		}
 	}
 }
---
+++ Assets/Scripts/WaveSpawnerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveSpawnerController : MonoBehaviour {
+	public GameObject[] enemies;
+	public float spawnRate;
+	public float delay;
+
+	void Start () {
+		InvokeRepeating ("SpawnEnemy", delay, spawnRate);
+	}
+	
+	// Update is called once per frame
+	void SpawnEnemy () {
+		Vector3 position = gameObject.transform.position;
+		position.x = Random.Range(-7.5f,7.5f);
+		Instantiate(enemies[(Random.Range(0, enemies.Length))], position, gameObject.transform.rotation);
+	}
+}
