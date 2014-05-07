6bc535fed442ebf7120e6f80fde529bedc052152
Andrés <angarg12@gmail.com>
2014-05-07 23:42:19 +0200

    Player controller logic centralized
    The logic for controlling hero and sidekick has been centrlized in a
    parent class.

--- Assets/Scripts/HeroController.cs
+++ Assets/Scripts/HeroController.cs
@@ -1,36 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class HeroController : MonoBehaviour {
-	public float speed;
-	public Boundary boundary;
-	
-	public BulletFaction shotPrefab;
-	public Transform shotSpawn;
-	public float fireRate;
-	
-	private float nextFire;
-	
-	void Update (){
-		if (Input.GetButton("Fire1") && Time.time > nextFire) 
-		{
-			nextFire = Time.time + fireRate;
-			Instantiate(shotPrefab, shotSpawn.position, shotSpawn.rotation);
-			//audio.Play ();
-		}
-	}
-	
-	void FixedUpdate (){
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
-		
-		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
-		rigidbody2D.velocity = movement * speed;
-
-		transform.position = new Vector3(
-			Mathf.Clamp (transform.position.x, boundary.xMin, boundary.xMax), 
-			Mathf.Clamp (transform.position.y, boundary.yMin, boundary.yMax),
-			0.0f
-			);
+public class HeroController : PlayerController {
+	void Start () {
+		FireButton = "Fire1";
+		HorizontalAxis = "Horizontal";
+		VerticalAxis = "Vertical";
 	}
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	public Boundary boundary;
+	
+	public BulletFaction shotPrefab;
+	public Transform shotSpawn;
+	public float fireRate;
+	
+	private float nextFire;
+	public string FireButton = "";
+	public string HorizontalAxis = "";
+	public string VerticalAxis = "";
+	
+	void Update (){
+		if (Input.GetButton(FireButton) && Time.time > nextFire) 
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(shotPrefab, shotSpawn.position, shotSpawn.rotation);
+			//audio.Play ();
+		}
+	}
+	
+	void FixedUpdate (){
+		float moveHorizontal = Input.GetAxis (HorizontalAxis);
+		float moveVertical = Input.GetAxis (VerticalAxis);
+		
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rigidbody2D.velocity = movement * speed;
+		
+		transform.position = new Vector3(
+			Mathf.Clamp (transform.position.x, boundary.xMin, boundary.xMax), 
+			Mathf.Clamp (transform.position.y, boundary.yMin, boundary.yMax),
+			0.0f
+			);
+	}
+}
