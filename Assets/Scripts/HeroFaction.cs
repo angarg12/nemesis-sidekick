4230c3aa40ca45338380777a1c94a949057b0176
Andrés <angarg12@gmail.com>
2014-05-08 00:33:09 +0200

    Explosion particle effect for hero
    Plays on death.

--- Assets/Scripts/HeroFaction.cs
+++ Assets/Scripts/HeroFaction.cs
@@ -2,9 +2,12 @@
 using System.Collections;
 
 public class HeroFaction : Faction {
+	public GameObject explosion;
+
 	public override void destroy(GameObject orderer){
 		if(orderer.tag == "enemy"){
 			Destroy(gameObject);
+			Instantiate(explosion, transform.position, transform.rotation);
 		}
 	}
 }
