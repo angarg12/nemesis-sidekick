54d365d2202eaad525c3602719c3baef03b278c0
Andrés <angarg12@gmail.com>
2014-05-09 22:35:10 +0200

    Basic GUI implemented
    A basic GUI that shows lives and score has been added.

---
+++ Assets/Scripts/LevelGUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelGUI : MonoBehaviour {
+	public GUIStyle style;
+
+	public PlayerController hero;
+	public PlayerController sidekick;
+
+	void OnGUI () {
+		GUI.Label (new Rect (20,10,100,50), "Hero", style);
+		GUI.Label (new Rect (20,30,100,50), "Lives: "+hero.lives, style);
+		GUI.Label (new Rect (20,50,100,50), "Points: 0", style);
+
+		
+		GUI.Label (new Rect (Screen.width - 150,10,100,50), "Sidekick", style);
+		GUI.Label (new Rect (Screen.width - 150,30,100,50), "Lives: "+sidekick.lives, style);
+		GUI.Label (new Rect (Screen.width - 150,50,100,50), "Points: 0", style);
+	}
+}
