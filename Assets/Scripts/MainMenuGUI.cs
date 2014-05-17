0bb5ed2325c7626056a2404ed0d3e36572446f5c
Andrés <angarg12@gmail.com>
2014-05-17 17:50:16 +0200

    Main menu added
    Added a main menu where you can select the game mode.

---
+++ Assets/Scripts/MainMenuGUI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuGUI : MonoBehaviour {
+	void OnGUI () {
+		GUI.skin.button.wordWrap = true;
+		if(GUI.Button(new Rect((Screen.width-200)/2-200,(Screen.height-500)/2,200,400), "Nemesis mode\n Competitive game\n Score 2000 total points and be the last player alive to win.")){
+			Application.LoadLevel("NemesisTest");
+		}
+		if(GUI.Button(new Rect((Screen.width-200)/2+200,(Screen.height-500)/2,200,400), "Sidekick mode\n Cooperative game\n Score 5000 total combined points and survive both players to win.")){
+			Application.LoadLevel("SidekickTest");
+		}
+		if(GUI.Button(new Rect((Screen.width-100)/2,(Screen.height+200)/2,100,50), "Quit")){
+			Application.Quit();
+		}
+	}
+}
