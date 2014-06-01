95ecf154092890697e9cdf1859525dc4bbf002f4
Andrés <angarg12@gmail.com>
2014-06-01 22:06:04 +0300

    Shooting audio overlapping solved
    An static variable has been added to shooter. Now before playing the
    sound, it checks that it hasn't been played in the last few
    milliseconds.

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -8,6 +8,11 @@ public class Shooter : MonoBehaviour {
 	public float delay;
 	public AudioClip fireAudio;
 
+	// FIXME: Is there a better method to prevent the audio from playing multiple times than
+	// using an static variable?
+	float audioPlayDelay = 0.2f;
+	static float LastTimeFired = 0f;
+
 	void Start () {
 		InvokeRepeating ("Fire", delay, fireRate);
 	}
@@ -15,7 +20,10 @@ public class Shooter : MonoBehaviour {
 	void Fire () {
 		BulletFaction shotInstance = (BulletFaction)Instantiate(shotPrefab, shotSpawn.position, shotSpawn.rotation);
 		shotInstance.setFather(gameObject);
-		AudioSource audio = GameObject.FindWithTag("GameController").GetComponent<AudioSource>();
-		audio.PlayOneShot(fireAudio, 0.2F);
+		if (Time.time > LastTimeFired + audioPlayDelay) {
+			AudioSource audio = GameObject.FindWithTag ("GameController").GetComponent<AudioSource> ();
+			audio.PlayOneShot (fireAudio, 0.2F);
+			LastTimeFired = Time.time;
+		}
 	}
 }
