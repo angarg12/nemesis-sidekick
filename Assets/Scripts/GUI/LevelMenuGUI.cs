c85a0847739740698ea8663f6ec5e835f87f6e18
Andrés <angarg12@gmail.com>
2014-06-06 22:06:40 +0300

    Basic level GUI
    Basic level GUI implemented.

---
+++ Assets/Scripts/GUI/LevelMenuGUI.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelMenuGUI : MonoBehaviour {
+	void OnGUI () {
+		string levelDescription = "";
+		GUILayout.BeginArea (
+			new Rect(
+				(Screen.width-200)/2-200,
+				(Screen.height-500)/2,
+				600,
+				600));
+		GUILayout.BeginHorizontal ();
+		if(GUILayout.Button("1", GUILayout.Height(40), GUILayout.Width(40))){
+			Application.LoadLevel("NemesisTest");
+		}
+		// What a convoluted way to implement the onHover function. Keep an eye for if they ever simplify this.
+		if(Event.current.type == EventType.Repaint &&
+		   GUILayoutUtility.GetLastRect().Contains(Event.current.mousePosition )){
+			levelDescription = "Score 2000 points to win.";
+		}
+
+		GUILayout.EndHorizontal ();
+		GUILayout.EndArea ();
+		// This box contains the description of the level. It changes as you hover over level buttons.
+		
+		GUI.skin.box.wordWrap = true;
+		GUI.Box(
+			new Rect(
+				(Screen.width-250),
+				(Screen.height-500)/2,
+				200,
+				400), 
+			levelDescription);
+		// Back to main menu
+		if(GUI.Button(new Rect((Screen.width-100)/2,(Screen.height+200)/2,100,50), "Back")){
+			Application.LoadLevel("MainMenu");
+		}
+	}
+}
--- Assets/Scripts/GUI/MainMenuGUI.cs
+++ Assets/Scripts/GUI/MainMenuGUI.cs
@@ -7,20 +7,24 @@ public class MainMenuGUI : MonoBehaviour {
 		if(GUI.Button(
 			new Rect(
 				(Screen.width-200)/2-200,
-				(Screen.height-500)/2,200,400), 
-				"Nemesis mode\n " +
-				"Competitive game\n " +
-				"Score 2000 total points and be the last player alive to win.")){
+				(Screen.height-500)/2,
+				200,
+				400), 
+			"Nemesis mode\n " +
+			"Competitive game\n " +
+			"Score 2000 total points and be the last player alive to win.")){
 			Application.LoadLevel("NemesisTest");
 		}
 		if(GUI.Button(
 			new Rect(
 				(Screen.width-200)/2+200,
-				(Screen.height-500)/2,200,400), 
-				"Sidekick mode\n " +
-				"Cooperative game\n " +
-				"Friendly fire is enabled\n " +
-				"Score 5000 total combined points and survive both players to win.")){
+				(Screen.height-500)/2,
+				200,
+				400), 
+			"Sidekick mode\n " +
+			"Cooperative game\n " +
+			"Friendly fire is enabled\n " +
+			"Score 5000 total combined points and survive both players to win.")){
 			Application.LoadLevel("SidekickTest");
 		}
 		if(GUI.Button(new Rect((Screen.width-100)/2,(Screen.height+200)/2,100,50), "Quit")){
