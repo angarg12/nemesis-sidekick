211454aa03ae37a36bc2b1f06fe26534b2a9bfa8
Andrés <angarg12@gmail.com>
2014-06-21 19:18:44 +0300

    First step of enemy movement
    The first step to implement enemy movement as a command processor.
    Currently has very limited functionality, as it only can process
    movements of a single type.

---
+++ Assets/Scripts/Unit/Behavior/MovementProcessor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using Newtonsoft.Json;
+
+public class MovementProcessor : MonoBehaviour {
+	Movement[] movements;
+
+	void Start () {
+		TextAsset fileContent = Resources.Load<TextAsset>("EnemyMovement");
+		movements = JsonConvert.DeserializeObject<MovementLine[]>(fileContent.text);
+		StartCoroutine(process());
+	}
+
+	IEnumerator process () {
+		foreach (Movement movement in movements) {
+			yield return StartCoroutine(movement.execute(gameObject));
+		}
+		enabled = false;
+	}
+}
